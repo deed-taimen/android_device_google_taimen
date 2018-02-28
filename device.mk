bfca2fc5fe25456c5025b6b2f6c163a7453c4fc6
Zips <hcolmenares@gmail.com>
2018-02-28 13:44:59 -0500

    taimen: include gapps
    Change-Id: I269019dca26d4539110044f4cd075c90acbebe5e

--- device.mk
+++ device.mk
@@ -138,3 +138,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Privileged app permissions
 PRODUCT_COPY_FILES += \
     device/google/taimen/privapp-permissions-taimen.xml:system/etc/permissions/privapp-permissions-taimen.xml
+
+#gapps blobs
+$(call inherit-product, vendor/pixelgapps/pixel-gapps.mk)

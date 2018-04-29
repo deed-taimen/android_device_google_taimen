92282354974d1df295ef2af8513fc81614cb3131
pimpmaneaton <shaunsibu@gmail.com>
2018-04-29 09:43:18 -0600

    Moved to my PixelGapps repo
    Change-Id: Ie43367bc6dbb62d6f2a361c6a8ae3d63905778c5

--- device.mk
+++ device.mk
@@ -47,10 +47,6 @@ PRODUCT_COPY_FILES += \
     device/google/taimen/init-taimen.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/init-$(PRODUCT_HARDWARE).rc \
     device/google/taimen/init.taimen.usb.rc:$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/init.$(PRODUCT_HARDWARE).usb.rc
 
-# Google Markup App
-PRODUCT_PACKAGES += \
-    Markup
-
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.sf.lcd_density=560 \
 

9df3f3c79f65b6c02912fe0f3ab89986f23f7b75
deed821 <deed821@gmail.com>
2018-05-20 19:10:09 +1000

    modify for lineageos
    Change-Id: I774f07d3ead966bc8e6efc5ff2288948ec571694

--- lineage.mk
+++ lineage.mk
@@ -2,8 +2,8 @@
 TARGET_SCREEN_HEIGHT := 2880
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common RR stuff.
-$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
@@ -11,21 +11,18 @@ $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 -include device/google/wahoo/device-lineage.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := rr_taimen
+PRODUCT_NAME := lineage_taimen
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 2 XL
 TARGET_MANUFACTURER := LG
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-
-# RR Device Maintainers
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="pimpmaneaton"
-
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
+    DEVICE_MAINTAINERS="Deedwar"
     BUILD_FINGERPRINT=google/taimen/taimen:8.1.0/OPM2.171019.029.B1/4657601:user/release-keys \
     PRIVATE_BUILD_DESC="taimen-user 8.1.0 OPM2.171019.029.B1 4657601 release-keys"
+
+BUILD_FINGERPRINT := google/taimen/taimen:8.1.0/OPM2.171019.029.B1/4657601:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)

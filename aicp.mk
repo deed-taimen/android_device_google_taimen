1ea6e21a1a80384b262b3c13487d2117d453a407
deed821 <deed821@gmail.com>
2018-05-26 12:58:12 +1000

    update build ID
    Change-Id: I71ebf2e118d80c583aa5729a8b63d4d794e60f43

--- aicp.mk
+++ aicp.mk
@@ -12,10 +12,10 @@ $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := aicp_taimen
-PRODUCT_BRAND := google
+PRODUCT_DEVICE := taimen
+PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel 2 XL
-TARGET_MANUFACTURER := LG
-PRODUCT_RESTRICT_VENDOR_FILES := false
+PRODUCT_MANUFACTURER := Google
 
 # AICP Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
@@ -23,7 +23,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
-    BUILD_FINGERPRINT=google/taimen/taimen:8.1.0/OPM2.171019.029.B1/4657601:user/release-keys \
-    PRIVATE_BUILD_DESC="taimen-user 8.1.0 OPM2.171019.029.B1 4657601 release-keys"
+    BUILD_FINGERPRINT=google/taimen/taimen:8.1.0/OPM2.171019.029.B1/4720900:user/release-keys \
+    PRIVATE_BUILD_DESC="taimen-user 8.1.0 OPM2.171019.029.B1 4720900 release-keys"
 
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)

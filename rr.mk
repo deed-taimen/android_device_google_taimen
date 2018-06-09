f834b36a106003dbbb241ba3857eeff471b6b55b
deed821 <deed821@gmail.com>
2018-06-09 18:42:40 +1000

    taimen: initial commit using https://github.com/CarbonROM as base

---
+++ rr.mk
@@ -0,0 +1,26 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 2880
+TARGET_SCREEN_WIDTH := 1440
+
+# Inherit some common RR stuff.
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/google/taimen/aosp_taimen.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := rr_taimen
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 2 XL
+TARGET_MANUFACTURER := LGE
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME=taimen \
+    BUILD_FINGERPRINT=google/taimen/taimen:8.1.0/OPM2.171026.006.C1/4769658:user/release-keys \
+    PRIVATE_BUILD_DESC="taimen-user 8.1.0 OPM2.171026.006.C1 4769658 release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    DEVICE_MAINTAINERS="deedwar"
+
+$(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)

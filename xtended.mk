381166a3cc2174f95f6d13c99eb3bd3fa85b7275
deed821 <deed821@gmail.com>
2018-07-06 21:31:08 +1000

    taimen: update for july

--- xtended.mk
+++ xtended.mk
@@ -2,14 +2,14 @@
 TARGET_SCREEN_HEIGHT := 2880
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common Validus stuff.
-$(call inherit-product, vendor/validus/config/common_full_phone.mk)
+# Inherit some common gzosp stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := validus_taimen
+PRODUCT_NAME := xtended_taimen
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 2 XL
 TARGET_MANUFACTURER := LGE
@@ -17,8 +17,8 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
-    BUILD_FINGERPRINT=google/taimen/taimen:8.1.0/OPM2.171026.006.C1/4769658:user/release-keys \
-    PRIVATE_BUILD_DESC="taimen-user 8.1.0 OPM2.171026.006.C1 4769658 release-keys"
+    BUILD_FINGERPRINT=google/taimen/taimen:8.1.0/OPM2.171026.006.H1/4833802:user/release-keys \
+    PRIVATE_BUILD_DESC="taimen-user 8.1.0 OPM2.171026.006.H1 4833802 release-keys"
 
 PRODUCT_PROPERTY_OVERRIDES += \
     DEVICE_MAINTAINERS="deedwar"

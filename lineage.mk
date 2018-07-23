5739a093977db55be83d016d486c4e0793246725
deed821 <deed821@gmail.com>
2018-07-23 16:49:54 +1000

    lineage: bringup for different ROM

--- lineage.mk
+++ lineage.mk
@@ -2,26 +2,22 @@
 TARGET_SCREEN_HEIGHT := 2880
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common RR stuff.
-$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 
--include device/google/wahoo/device-lineage.mk
-
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := rr_taimen
+PRODUCT_NAME := lineage_taimen
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 2 XL
 TARGET_MANUFACTURER := LG
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-
-# RR Device Maintainers
+# Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="pimpmaneaton"
-
+    DEVICE_MAINTAINERS="deedwar"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \

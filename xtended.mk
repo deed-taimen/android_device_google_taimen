ed710e51ac6ba529e970b0be9b39d1373191e8b8
deed821 <deed821@gmail.com>
2018-07-26 04:23:28 +1000

    xtended: update for a new ROM

--- xtended.mk
+++ xtended.mk
@@ -2,8 +2,8 @@
 TARGET_SCREEN_HEIGHT := 2880
 TARGET_SCREEN_WIDTH := 1440
 
-# Inherit some common RR stuff.
-$(call inherit-product, vendor/rr/config/common_full_phone.mk)
+# Inherit some common stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
@@ -11,17 +11,15 @@ $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 -include device/google/wahoo/device-lineage.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := rr_taimen
+PRODUCT_NAME := xtended_taimen
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Pixel 2 XL
 TARGET_MANUFACTURER := LG
 PRODUCT_RESTRICT_VENDOR_FILES := false
 
-
-# RR Device Maintainers
+# Device Maintainers
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    DEVICE_MAINTAINERS="pimpmaneaton"
-
+    DEVICE_MAINTAINERS="deedwar"
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=taimen \
@@ -30,3 +28,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := google/taimen/taimen:8.1.0/OPM2.171026.006.H1/4833802:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
+$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+

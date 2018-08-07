914a9cf493515faa046442444626c4b783a55638
deed821 <deed821@gmail.com>
2018-08-07 17:31:41 +1000

    validus: change mk file for different ROM

--- validus.mk
+++ validus.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 2880
 TARGET_SCREEN_WIDTH := 1440
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
+$(call inherit-product, vendor/validus/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/google/taimen/aosp_taimen.mk)
@@ -11,8 +11,8 @@ $(call inherit-product, device/google/taimen/aosp_taimen.mk)
 -include device/google/wahoo/device-lineage.mk
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := xtended_taimen
-PRODUCT_BRAND := google
+PRODUCT_NAME := validus_taimen
+PRODUCT_BRAND := Google
 PRODUCT_MODEL := Pixel 2 XL
 TARGET_MANUFACTURER := LG
 PRODUCT_RESTRICT_VENDOR_FILES := false
@@ -28,5 +28,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 BUILD_FINGERPRINT := google/taimen/taimen:8.1.0/OPM2.171026.006.H1/4833802:user/release-keys
 
 $(call inherit-product-if-exists, vendor/google/taimen/taimen-vendor.mk)
-$(call inherit-product-if-exists, vendor/pixelgapps/pixel-gapps.mk)
+$(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
 
+GAPPS_VARIANT := micro
+GAPPS_EXCLUDED_PACKAGES := Hangouts
